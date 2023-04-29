21519e0434eae31d6416234450bd87a975d0b6be
Amer Khaled <amrkhald144@gmail.com>
2023-04-29 19:11:29 +0300

    Homework 1

---
+++ 1-First Term/Unit2-C Programming/Lesson 3/Homework 1/Ex4/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    float num1;
+    float num2;
+
+    printf("Enter two numbers: ");
+    scanf("%f%f", &num1, &num2);
+
+    printf("product: %f", num1 * num2);
+
+
+    return 0;
+}
