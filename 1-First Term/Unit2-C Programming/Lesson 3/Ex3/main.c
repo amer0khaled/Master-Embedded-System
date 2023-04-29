a2ddd9d0a756639ada5893d59dc937c737462e18
Amer Khaled <amrkhald144@gmail.com>
2023-04-29 17:09:00 +0300

    Homework1__Ex3

---
+++ 1-First Term/Unit2-C Programming/Lesson 3/Ex3/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+
+    printf("Enter two integers: ");
+
+    scanf("%i%i", &num1, &num2);
+
+    printf("Sum: %i", num1+num2);
+
+
+    return 0;
+}
