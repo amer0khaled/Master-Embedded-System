2f62474f67d5eeabdeb489458708bee1bbdd8709
Amer Khaled <amrkhald144@gmail.com>
2023-04-29 21:33:39 +0300

    Homework2__Ex4

---
+++ 1-First Term/Unit2-C Programming/Lesson 3/Homework 2/Ex4/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+#define READ_BIT(NUM, BIT) NUM&(1<<BIT)
+
+int main()
+{
+   float num;
+
+   printf("Enter a number: ");
+
+   scanf("%f", &num);
+
+   if(num < 0)
+   {
+        printf("%f is negative.", num);
+   }
+   else if (num > 0)
+   {
+        printf("%f is positive.", num);
+   }
+   else
+   {
+        printf("You entered zero.");
+   }
+   
+    
+
+    return 0;
+}
